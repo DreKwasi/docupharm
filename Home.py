27c0fd36e9d5ef000a417dca863dc97c5002f39b
DreKwasi <andrewsboateng137@gmail.com>
2023-01-26 22:29:38 +0000

    Changed Register Auth

--- Home.py
+++ Home.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import streamlit_authenticator as st_auth
 from streamlit_option_menu import option_menu
-from helper_funcs import db, home_func, styles
+from helper_funcs import db, home_func, styles, register_user
 from streamlit_extras.switch_page_button import switch_page
+
 
 st.set_page_config(
     page_title="DocuPharm",
@@ -10,7 +11,6 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
     layout="centered",
 )
-
 
 styles.load_css_file("styles/main.css")
 
@@ -32,89 +32,69 @@ placeholder = st.empty()
 msg_holder = st.empty()
 
 
-if st.session_state["username"]:
-    if  db.check_profile(st.session_state["username"]):
-        st.sidebar.subheader(
-            """Welcome to DocuPharm \n The #1 Impact Tracker for Pharmacists"""
-        )
+with placeholder.container():
+    options = ["Sign In", "Sign Up"]
+    selected = option_menu(menu_title="", options=options, orientation="horizontal")
+    
+    st.header("Welcome to DocuPharm 🎈🎈🎈")
+    st.subheader("The #1 Impact Tracker for Pharmacists 👨‍⚕️👩‍⚕️")
 
-        st.header(f'Hello,  {st.session_state["name"]} :grinning:')
-        st.session_state["authenticator"] = authenticator
-        with st.sidebar.empty():
-                authenticator.logout("Logout", "main")
-        
-        home_func.show_dashboard()
-        
-    else:
-        switch_page("my profile")
+    if "success_message" in st.session_state:
+        st.success(st.session_state["success_message"])
 
-else:
-    with placeholder.container():
-        options = ["Sign In", "Sign Up"]
-        selected = option_menu(menu_title="", options=options, orientation="horizontal")
-        st.header("Welcome to DocuPharm 🎈🎈🎈")
-        st.subheader("The #1 Impact Tracker for Pharmacists 👨‍⚕️👩‍⚕️")
+if selected == "Sign In":
+    # if "refresh" in st.session_state:
+    #     del st.session_state["refresh"]
 
-        if "success_message" in st.session_state:
-            st.success(st.session_state["success_message"])
+    name, authentication_status, username = authenticator.login("Sign In", "main")
 
-
-    if selected == "Sign In":
-        # if "refresh" in st.session_state:
-        #     del st.session_state["refresh"]
-            
-        name, authentication_status, username = authenticator.login("Sign In", "main")
-
-        if authentication_status:
-            
-            (
-                st.session_state["name"],
-                st.session_state["authentication_status"],
-                st.session_state["username"],
-            ) = (
-                name,
-                authentication_status,
-                username,
-            )
-
-            st.session_state["authenticator"] = authenticator
-
-            with st.sidebar.empty():
-                authenticator.logout("Logout", "main")
-                
-            placeholder.empty()
-            if st.session_state["username"] and db.check_profile(
-                st.session_state["username"]
-            ):
-
-                st.sidebar.subheader(
-                    """Welcome to DocuPharm \n The #1 Impact Tracker for Pharmacists"""
-                )
-
-                st.header(f'Hello,  {st.session_state["name"]} :grinning:')
-                home_func.show_dashboard()
-                
-            else:
-                switch_page("my profile")
-
-        elif authentication_status == False:
-            st.error("Username/password is incorrect")
-
-        elif authentication_status == None:
-            st.warning("Please enter your username and password")
-
-    elif selected == "Sign Up":
-        msg_holder.empty()
+    if authentication_status:
         if "success_message" in st.session_state:
             del st.session_state["success_message"]
-        try:
-            if authenticator.register_user("Get Started 🧾", preauthorization=False):
-                db.create_user(
-                    usernames=creds["usernames"],
-                )
-                st.session_state[
-                    "success_message"
-                ] = "User registered successfully. Please Log In"
-                switch_page("my profile")
-        except Exception as e:
-            st.error(e)
+            
+        (
+            st.session_state["name"],
+            st.session_state["authentication_status"],
+            st.session_state["username"],
+        ) = (
+            name,
+            authentication_status,
+            username,
+        )
+
+        st.session_state["authenticator"] = authenticator
+
+        with st.sidebar.empty():
+            authenticator.logout("Logout", "main")
+
+        placeholder.empty()
+        if st.session_state["username"] and db.check_profile(
+            st.session_state["username"]
+        ):
+
+            st.sidebar.subheader(
+                """Welcome to DocuPharm \n The #1 Impact Tracker for Pharmacists"""
+            )
+
+            st.header(f'Hello,  {st.session_state["name"]} :grinning:')
+            home_func.show_dashboard()
+
+        else:
+            switch_page("my profile")
+
+    elif authentication_status == False:
+        st.error("Username/password is incorrect")
+
+    elif authentication_status == None:
+        st.warning("Please enter your username and password")
+
+elif selected == "Sign Up":
+    msg_holder.empty()
+    if "success_message" in st.session_state:
+        del st.session_state["success_message"]
+
+    if register_user.register(creds):
+        st.session_state[
+            "success_message"
+        ] = "User registered successfully. Please Log In"
+        switch_page("profile")
