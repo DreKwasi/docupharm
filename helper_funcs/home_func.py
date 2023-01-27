426eee257477b3ed8d8c263b57b37821ed8a6af8
DreKwasi <andrewsboateng137@gmail.com>
2023-01-27 21:08:08 +0000

    Loading for Dashboard

--- helper_funcs/home_func.py
+++ helper_funcs/home_func.py
@@ -33,35 +33,36 @@ def show_dashboard():
     #     height=0,
     #     width=0,
     # )
-    df_patient, df_intv = get_dashboard_data(st.session_state["username"])
-    if not df_patient.empty and not df_intv.empty:
-        df_patient.merge(
-            df_intv, how="inner", left_on="intervention_key", right_on="key"
-        )
-
-    patients = df_patient.shape[0]
-    interventions = df_intv.shape[0]
-
-    metric1Col1, metric1Col2 = st.columns([1, 1])
-
-    metric1Col1.metric("**Interventions**", interventions)
-    metric1Col2.metric("**Patients Impacted**", patients)
-    # metric1Col3.metric("**Prescriptions Filled**", 30, "20%")
-    style_metric_cards()
-
-    st.write("####")
-
-    if interventions > 0:
-        group_df = df_intv.groupby(["pharmaceutical_care", "recorded_date"]).agg(
-            Count=("recorded_date", "count")
-        ).reset_index()
-
-        fig = px.bar(group_df, x="recorded_date", y="Count", color="pharmaceutical_care")
-        with fig.batch_update():
-            fig.update_layout(coloraxis_showscale=False)
-            fig.update_layout(
-                title="Interventions Served",
+    with st.spinner("Loading..."):
+        df_patient, df_intv = get_dashboard_data(st.session_state["username"])
+        if not df_patient.empty and not df_intv.empty:
+            df_patient.merge(
+                df_intv, how="inner", left_on="intervention_key", right_on="key"
             )
-        st.plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
-    else:
-        st.info("You Have Not Served Any Interventions")
+
+        patients = df_patient.shape[0]
+        interventions = df_intv.shape[0]
+
+        metric1Col1, metric1Col2 = st.columns([1, 1])
+
+        metric1Col1.metric("**Interventions**", interventions)
+        metric1Col2.metric("**Patients Impacted**", patients)
+        # metric1Col3.metric("**Prescriptions Filled**", 30, "20%")
+        style_metric_cards()
+
+        st.write("####")
+
+        if interventions > 0:
+            group_df = df_intv.groupby(["pharmaceutical_care", "recorded_date"]).agg(
+                Count=("recorded_date", "count")
+            ).reset_index()
+
+            fig = px.bar(group_df, x="recorded_date", y="Count", color="pharmaceutical_care")
+            with fig.batch_update():
+                fig.update_layout(coloraxis_showscale=False)
+                fig.update_layout(
+                    title="Interventions Served",
+                )
+            st.plotly_chart(fig, use_container_width=True, config={"displayModeBar": False})
+        else:
+            st.info("You Have Not Served Any Interventions")
