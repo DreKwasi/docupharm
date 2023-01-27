9ef1610ee518f80deab713b863d2153ecb03022d
DreKwasi <andrewsboateng137@gmail.com>
2023-01-27 23:38:04 +0000

    Renamed page

--- pages/2_My_Interventions.py
+++ pages/2_My_Interventions.py
@@ -13,6 +13,8 @@ st.set_page_config(
 )
 
 page_util.delete_page("Home", "Add_My_Intervention")
+page_util.delete_page("Home", "Add_My_Patient")
+
 
 # st.session_state["refresh"] = "not refresh"
 # st.experimental_set_query_params(curr_page="my intervention")
