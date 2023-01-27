ee00c6e58129beee697ffea63246ccb637f73be8
DreKwasi <andrewsboateng137@gmail.com>
2023-01-27 19:40:44 +0000

    Validation Logic for Intervention & Patient Forms

---
+++ helper_funcs/validators.py
@@ -0,0 +1,65 @@
+import streamlit as st
+
+
+def validate_intervention(details, error_placeholder):
+
+    if details["pharmaceutical_care"]:
+        if details["pharmaceutical_care_details"]:
+            if details["medications"]:
+                if details["intervention"]:
+                    return True
+                else:
+                    error_placeholder.error("Intervention Can Not Be Empty")
+            else:
+                error_placeholder.error("Select At Least 1 Medication")
+        else:
+            error_placeholder.error(
+                "Give At least 1 Detail about the Pharmaceutical Care"
+            )
+    else:
+        error_placeholder.error("Select A Pharmaceutical Care")
+
+
+def validate_patient(details, error_placeholder, no_details=False):
+
+    if not no_details:
+        if details["patient_name"]:
+            if details["phone_number"]:
+                if details["gender"]:
+                    return True
+                else:
+                    error_placeholder.error("Indicate Patient's Gender")
+            else:
+                error_placeholder.error("Indicate Patient's Phone Number")
+        else:
+            error_placeholder.error("Patient Must Have A Name")
+
+    if no_details:
+        if details["gender"]:
+            return True
+        else:
+            error_placeholder.error("Indicate Patient's Gender")
+
+
+# def validate_profile(details, error_placeholder):
+
+#     if details["first_name"]:
+#         if details["last_name"]:
+#             if details["status"]:
+#                 if details["reg_number"]:
+#                     if (
+#                         len(details["work_details"]["company"]) > 0
+#                         and (details["work_details"]["location"]) > 0
+#                         and (details["work_details"]["category"]) > 0
+#                     ):
+#                         return True
+#                     else:
+#                         error_placeholder.error("Indicate At Least One Work Detail")
+#                 else:
+#                     error_placeholder.error("Indicate Registration Number")
+#             else:
+#                 error_placeholder.error("Indicate Your Professional Status")
+#         else:
+#             error_placeholder.error("Please Input Your Last Name")
+#     else:
+#         error_placeholder.error("Please Input Your First Name")
